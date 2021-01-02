e99260e86b62708727063315344a33073a9b4721
Ilya Rogatkin <toch.in.go@googlemail.com>
2021-01-02 21:17:41 +0300

    add test sources

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// #include "./TutorialConfig.h"
+
+
+int main(int argc, char const *argv[]) {
+    // if(argc < 2) {
+        // report version
+        // cout << argv[0] << " Version " << Tutorial_VERSION_MAJOR << " . "<< Tutorial_VERSION_MINOR << endl;
+        // cout << "Usage: " << argv[0] << " number" << endl;
+
+    // }
+    cout << "Hello World";
+    return 0;
+}
